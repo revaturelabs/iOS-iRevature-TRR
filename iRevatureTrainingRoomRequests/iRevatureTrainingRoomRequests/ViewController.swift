8de68e616e37edb4e01dbee5fcedae762f815468
Nathan Coggins <25321608+NCOGGINS@users.noreply.github.com>
2020-02-11 15:47:34 -0500

    Test Upload

---
+++ iRevatureTrainingRoomRequests/iRevatureTrainingRoomRequests/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iRevatureTrainingRoomRequests
+//
+//  Created by Nathan Coggins on 2/11/20.
+//  Copyright © 2020 revature. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iRevatureTrainingRoomRequests/iRevatureTrainingRoomRequestsUITests/iRevatureTrainingRoomRequestsUITests.swift
@@ -0,0 +1,36 @@
+//
+//  iRevatureTrainingRoomRequestsUITests.swift
+//  iRevatureTrainingRoomRequestsUITests
+//
+//  Created by Nathan Coggins on 2/11/20.
+//  Copyright © 2020 revature. All rights reserved.
+//
+
+import XCTest
+
+class iRevatureTrainingRoomRequestsUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
