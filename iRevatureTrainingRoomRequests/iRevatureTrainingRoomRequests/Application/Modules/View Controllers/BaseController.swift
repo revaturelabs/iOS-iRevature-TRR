82f306c912c7aa82bf402a3fcc1014f0e176f6c0
Nathan Coggins <25321608+NCOGGINS@users.noreply.github.com>
2020-02-14 14:00:07 -0500

    Created Base Controller

---
+++ iRevatureTrainingRoomRequests/iRevatureTrainingRoomRequests/Application/Modules/View Controllers/BaseController.swift
@@ -0,0 +1,16 @@
+//
+//  BaseController.swift
+//  iRevatureTrainingRoomRequests
+//
+//  Created by Nathan Coggins on 2/14/20.
+//  Copyright © 2020 revature. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BaseController: UIViewController {
+    func checkTokenStatus(){
+        //TODO
+    }
+}
