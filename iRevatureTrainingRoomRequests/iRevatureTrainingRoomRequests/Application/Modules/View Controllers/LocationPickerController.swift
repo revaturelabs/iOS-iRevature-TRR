4c789b7b297a56fdf0c4184b23f0f37e3cadc6dc
Kjay Shieu <kxs646@case.edu>
2020-02-18 11:40:05 -0500

    fixed home button

--- iRevatureTrainingRoomRequests/iRevatureTrainingRoomRequests/Application/Modules/View Controllers/LocationPickerController.swift
+++ iRevatureTrainingRoomRequests/iRevatureTrainingRoomRequests/Application/Modules/View Controllers/LocationPickerController.swift
@@ -18,7 +18,12 @@ class LocationPickerController: BaseController {
     
     @IBAction func logOutButton(_ sender: Any) {
         //present(backVC, animated: false, completion: nil)
-        dismiss(animated: false, completion: nil)
+        let storyboard = UIStoryboard(name: "Login", bundle: nil)
+        
+        // Instantiate the desired view controller from the storyboard using the view controllers identifier
+        // Cast is as the custom view controller type you created in order to access it's properties and methods
+        let customViewController = storyboard.instantiateViewController(withIdentifier: "HomeScreen")
+        present(customViewController, animated: false, completion: nil)
     }
     
     @IBAction func submitButton(_ sender: Any) {
