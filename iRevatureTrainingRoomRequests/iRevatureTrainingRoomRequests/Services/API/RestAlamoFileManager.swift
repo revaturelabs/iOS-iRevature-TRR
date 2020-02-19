3b6b2983fe05c100b832cc1c5d2071cc025ddc31
Kjay Shieu <kxs646@case.edu>
2020-02-19 12:26:32 -0500

    made RestAlamoFileManager file

---
+++ iRevatureTrainingRoomRequests/iRevatureTrainingRoomRequests/Services/API/RestAlamoFileManager.swift
@@ -0,0 +1,9 @@
+//
+//  RestAlamoFileManager.swift
+//  iRevatureTrainingRoomRequests
+//
+//  Created by admin on 2/19/20.
+//  Copyright © 2020 revature. All rights reserved.
+//
+
+import Foundation
