ef1e6ef980744d80f22c4b3e7e429bb0d109d515
Kjay Shieu <kxs646@case.edu>
2020-02-20 14:26:37 -0500

    updated Structs and enums to be Codable
    fixed the enums

--- iRevatureTrainingRoomRequests/iRevatureTrainingRoomRequests/Business/Entities/RequestType.swift
+++ iRevatureTrainingRoomRequests/iRevatureTrainingRoomRequests/Business/Entities/RequestType.swift
@@ -12,3 +12,39 @@ enum RequestType{
     case assign
     case unassign
 }
+
+
+extension RequestType: Codable {
+    
+    enum Key: CodingKey {
+        case rawValue
+    }
+    
+    enum CodingError: Error {
+        case unknownValue
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Key.self)
+        let rawValue = try container.decode(Int.self, forKey: .rawValue)
+        switch rawValue {
+        case 0:
+            self = .assign
+        case 1:
+            self = .unassign
+        default:
+            throw CodingError.unknownValue
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: Key.self)
+        switch self {
+        case .assign:
+            try container.encode(0, forKey: .rawValue)
+        case .unassign:
+            try container.encode(1, forKey: .rawValue)
+        }
+    }
+    
+}
--- iRevatureTrainingRoomRequests/iRevatureTrainingRoomRequests/Business/Entities/Status.swift
+++ iRevatureTrainingRoomRequests/iRevatureTrainingRoomRequests/Business/Entities/Status.swift
@@ -13,3 +13,43 @@ enum Status{
     case assigned
     case unassigned
 }
+
+
+extension Status: Codable {
+    
+    enum Key: CodingKey {
+        case rawValue
+    }
+    
+    enum CodingError: Error {
+        case unknownValue
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Key.self)
+        let rawValue = try container.decode(Int.self, forKey: .rawValue)
+        switch rawValue {
+        case 0:
+            self = .pending
+        case 1:
+            self = .assigned
+        case 2:
+            self = .unassigned
+        default:
+            throw CodingError.unknownValue
+        }
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: Key.self)
+        switch self {
+        case .pending:
+            try container.encode(0, forKey: .rawValue)
+        case .assigned:
+            try container.encode(1, forKey: .rawValue)
+        case .unassigned:
+            try container.encode(2, forKey: .rawValue)
+        }
+    }
+    
+}
