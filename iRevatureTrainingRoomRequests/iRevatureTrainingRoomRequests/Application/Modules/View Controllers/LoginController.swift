baf3809d98c82d201197ba2ba3e3f87979cd1125
Nathan Coggins <nthndcoggins@gmail.com>
2020-02-24 10:32:31 -0500

    Room picker defaults to all rooms.

--- iRevatureTrainingRoomRequests/iRevatureTrainingRoomRequests/Application/Modules/View Controllers/LoginController.swift
+++ iRevatureTrainingRoomRequests/iRevatureTrainingRoomRequests/Application/Modules/View Controllers/LoginController.swift
@@ -14,7 +14,6 @@ class LoginController: BaseController {
 
 
     @IBOutlet weak var keepMeLoggedSwitch: UISwitch!
-    
     @IBOutlet weak var failureMsgLabel: UILabel!
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
